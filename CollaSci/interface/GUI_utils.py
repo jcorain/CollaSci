33514f1f1f68f515365a0155a434b89a61fc17db
jcorain <jean.christophe.orain@gmail.com>
2022-10-25 21:34:08 +0200

    WIP : add add column button with callback for user table

--- CollaSci/interface/GUI_utils.py
+++ CollaSci/interface/GUI_utils.py
@@ -65,10 +65,74 @@ def delete_popup(parent, connection, table_name):
             
         popup.destroy()
         
-        
-    
     ok_button = tk.Button(popup, text = 'Delete column', command = lambda : delete_col(parent, connection, table_name))
     ok_button.pack()
+    
+def add_popup(parent, connection, table_name, parent_parent):
+    
+    # define the popup window
+    
+    popup = tk.Toplevel()
+    popup.title('Add a column')
+    
+    if table_name == 'user':
+        tab_user.UserAdd(popup, connection, parent_parent)
+        
+        
+    
+    # get the label
+    
+    # del_label = tk.Label(popup, text = 'Column id to delete')
+    # del_label.pack()
+    
+    # # define the first string value 
+    
+    # mystring = tk.StringVar(popup)
+    
+    # # get the entry for col number 
+    
+    # id_entry = tk.Entry(popup, textvariable = mystring)
+    # id_entry.pack()
+    
+    # # define the callback from button 
+    
+    # def delete_col(parent, connection, table_name):
+        
+    #     # get thepossible id values to delete 
+        
+    #     existing_id = []
+    #     existing_id_tup = database_utils.fetchall_query(connection, 'SELECT id FROM {}'.format(table_name))
+    #     for val in existing_id_tup:
+    #         existing_id = existing_id + [str(val[0])]
+
+    #     # if the id to delete is in the possible value delete it 
+    #     # else get a warning message
+
+    #     if mystring.get() in existing_id:
+    #         confirmation = tk.messagebox.askokcancel('','The colum id number {} is going to be deleted.'.format(mystring.get()))
+    #         if confirmation:
+    #             database_utils.delete_id_from_table(connection, table_name, int(mystring.get()))
+                
+    #             # to updtae the treeview, delete it and then redo it 
+                
+    #             for widget in parent.winfo_children():
+    #                 # delete delete button
+    #                 if '!button' in str(widget):
+    #                     widget.destroy()
+                    
+    #                 # delte treview and recreate it 
+    #                 if 'treeview' in str(widget):
+    #                     widget.destroy()
+    #                     if table_name == 'user':
+    #                         tab_user.UserTree(parent, connection)                
+            
+    #     else:
+    #         tk.messagebox.showinfo('','The colum id number {} does not exist. Please select another id value.'.format(mystring.get()))
+            
+    #     popup.destroy()
+        
+    # ok_button = tk.Button(popup, text = 'Delete column', command = lambda : delete_col(parent, connection, table_name))
+    # ok_button.pack()
 
 
 class DeleteButton(tk.Frame):
@@ -81,3 +145,13 @@ class DeleteButton(tk.Frame):
         
         self.label_del = tk.Button(parent, text = 'Delete a column', command = lambda : delete_popup(parent, connection, table_name))
         self.label_del.pack()
+        
+class AddButton(tk.Frame):
+    def __init__(self, parent, connection, table_name, parent_parent):
+        # define the frame for the delete buttton
+        tk.Frame.__init__(self, parent)
+        
+        # define the first label holding the already defined users 
+        
+        self.label_add = tk.Button(parent, text = 'Add a column', command = lambda : add_popup(parent, connection, table_name, parent_parent))
+        self.label_add.pack()
--- CollaSci/interface/tab_user.py
+++ CollaSci/interface/tab_user.py
@@ -5,9 +5,8 @@ import tkinter as tk
 from tkinter import ttk
 
 import CollaSci.db_function.database_utils as database_utils
+import CollaSci.db_function.tables_update as table_update
 import CollaSci.interface.GUI_utils as GUI_utils
-
-
 
 class UserWidget(tk.Frame):
     def __init__(self, parent, connection):
@@ -65,6 +64,7 @@ class UserTree():
                 # add the delete frame 
                 
                 GUI_utils.DeleteButton(parent, connection, 'user')
+                GUI_utils.AddButton(parent, connection, 'user', self)
 
                 
             else:
@@ -73,4 +73,96 @@ class UserTree():
         else:
             self.label_no_user_data = tk.Label(parent, text = 'There is no SQL connection')
             self.label_no_user_data.pack()
-         
+            
+class UserAdd():
+    def __init__(self, parent, connection, parent_parent):
+        if connection is not None:
+            # add the first name values 
+            
+            self.firstname = tk.Label(parent, text = 'First Name')
+            self.firstname.pack()
+            
+            # define the firstname string value 
+            
+            firstname = tk.StringVar(parent)
+            
+            # get the entry for firstname 
+           
+            self.firstname_entry = tk.Entry(parent, textvariable = firstname)
+            self.firstname_entry.pack()
+            
+            # add the first name values 
+            self.lastname = tk.Label(parent, text = 'Last Name')
+            self.lastname.pack()
+            
+            #define the firstname string value 
+           
+            lastname = tk.StringVar(parent)
+            
+            # get the entry for lastname 
+           
+            self.lastname_entry = tk.Entry(parent, textvariable = lastname)
+            self.lastname_entry.pack()
+            
+            # add the status values 
+            
+            self.status = tk.Label(parent, text = 'Status (if the status you want does not exist in the list please update the status table.')
+            self.status.pack()
+            
+            #define the status string value 
+           
+            status = tk.StringVar(parent)
+            
+            # get the list of existing status 
+            status_val = [val[0] for val in database_utils.fetchall_query(connection, 'SELECT name FROM status')]
+ 
+            self.statuslist = ttk.Combobox(parent, values = status_val, textvariable = status)
+            self.statuslist.set("Pick a status")
+            self.statuslist.pack()
+            
+            # add the laboratory values 
+            
+            self.laboratory = tk.Label(parent, text = 'Laboratory (if the laboratory you want does not exist in the list please update the laboratory table.')
+            self.laboratory.pack()
+            
+            #define the laboratory string value 
+           
+            laboratory = tk.StringVar(parent)
+            # get the list of existing status 
+            laboratory_val = [val[0] for val in database_utils.fetchall_query(connection, 'SELECT name FROM laboratory')]
+ 
+            self.laboratorylist = ttk.Combobox(parent, values = laboratory_val, textvariable = laboratory)
+            self.laboratorylist.set("Pick a laboratory")
+            self.laboratorylist.pack()
+            
+            # add the add button 
+            
+            self.add_button = tk.Button(parent, text = 'Add user column', command = lambda : add_user_col(parent, firstname, lastname, status, laboratory, connection, parent_parent))
+            self.add_button.pack()
+        
+def add_user_col(parent, firstname, lastname, status, laboratory, connection, parent_parent):
+    
+    # get the status id 
+    status_id = database_utils.fetchall_query(connection, 'SELECT id FROM status WHERE name = "{}"'.format(status.get()))[0][0]
+    
+    # get the laboratory id 
+    
+    laboratory_id = database_utils.fetchall_query(connection, 'SELECT id FROM laboratory WHERE name = "{}"'.format(laboratory.get()))[0][0]
+    
+    table_update.add_row_user_table(firstname.get(), lastname.get(), status_id, laboratory_id, connection)
+    
+    # to updtae the treeview, delete it and then redo it 
+    
+    for widget in parent_parent.winfo_children():
+        print(str(widget))
+        # # delete delete button
+        # if '!button' in str(widget):
+        #     widget.destroy()
+        
+        # # delte treview and recreate it 
+        # if 'treeview' in str(widget):
+        #     widget.destroy()
+        #     UserTree(parent, connection)                
+    
+    parent.destroy()
+        
